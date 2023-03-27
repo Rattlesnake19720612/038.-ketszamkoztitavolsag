718926a55fb70bb258d09796f7a73d93488d7b25
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-27 19:16:15 +0200

    Két szám közti távolság

---
+++ 038. ketszamkoztitavolsag/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _038.ketszamkoztitavolsag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Kérek egy számot!");
+            int szam1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérek még egy számot!");
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+
+            double tavolsag = Math.Abs(szam1 - szam2);
+
+            Console.Write("A két szám közti távolság: " + tavolsag);
+
+            Console.ReadKey();    
+        }
+    }
+}

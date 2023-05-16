cfbae794b432440e7ca115d0b0db2f6c9f7f3d5c
DGYKKSZKI <124397426+Rattlesnake19720612@users.noreply.github.com>
2023-05-17 00:05:30 +0200

    Két szám közti távolság

--- 038. ketszamkoztitavolsag/Program.cs
+++ 038. ketszamkoztitavolsag/Program.cs
@@ -21,3 +21,5 @@ namespace _038.ketszamkoztitavolsag
         }
     }
 }
+
+/*Ez a C# program kiszámítja két szám közötti távolságot. A felhasználótól bekéri a két számot, majd a Math.Abs() függvény segítségével kiszámítja a számok abszolút értékét, majd elmenti a távolságot egy double típusú változóba. Végül kiírja a távolságot a konzolra a Console.WriteLine() függvénnyel. A program végén a Console.ReadKey() függvény segítségével várakozik a felhasználó bemenetére, mielőtt bezáródna.*/
